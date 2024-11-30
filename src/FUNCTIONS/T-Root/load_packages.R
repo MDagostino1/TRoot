660c7b1598b95dadf87ca6f11a5d756068bc6fcd
NedHenry22 <dagostinomrc@gmail.com>
2024-11-30 15:40:57 +0100

    Updates of codes

--- src/FUNCTIONS/T-Root/load_packages.R
+++ src/FUNCTIONS/T-Root/load_packages.R
@@ -1,8 +1,8 @@
 load_packages <- function(pkg_list){
   for(i in pkg_list){
     if (!requireNamespace(i, quietly = TRUE)) {
+      try(install.packages(i, dependencies = TRUE))
       # If not installed, install the package
-      install.packages(i, dependencies = TRUE)
     }
     # Load the package
     library(i, character.only = TRUE)  
